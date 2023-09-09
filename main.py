af22e23e8786c6b8a0fa47fcb7f4e9501c5acda1
eşref can <144523909+EsrefCanSener@users.noreply.github.com>
2023-09-09 12:16:16 +0300

    Create main.py
    ders 1

---
+++ main.py
@@ -0,0 +1,14 @@
+meme_dict={
+    "CRINGE":"Garip ya da utandırıcı bir şey",
+    "LOL":"Komik bir şeye verilen cevap",
+    "ROFL": "bir şakaya karşılık cevap",
+    "SHEESH": "onaylamamak",
+    "CREEPY": "korkunç",
+    "AGGRO": "agresifleşmek/sinirlenmek"
+}
+while True:
+word = input("kelimeyi girin:")
+if word in meme_dict.keys():
+   print(meme_dict[word])
+else:
+   print("KELİMENİN KARŞILIĞI YOK")
